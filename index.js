bac108e82542fbf2c90438b7aaebb0d2b5424e33
Konrad Krasocki <104936644+KodeRad@users.noreply.github.com>
2023-08-15 17:02:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`));
+
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "👔",
+    size: "large",
+  });
+});
+
+app.post("/tshirt/:id", (req, res) => {
+  // we accept data from request and destructure it
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: "We need a logo!" });
+  }
+
+  res.send({
+    tshirt: `tshirt: ${logo} with the ID of ${id}`,
+  });
+});
